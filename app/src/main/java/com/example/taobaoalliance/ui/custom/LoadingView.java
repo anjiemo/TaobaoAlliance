74975c5038af47abe5dc6d2e62f9f44ceeb1709f
anjiemo <26957348416@qq.com>
2020-06-27 17:42:44 +0800

    编写LoadingView（第60集）。

---
+++ app/src/main/java/com/example/taobaoalliance/ui/custom/LoadingView.java
@@ -0,0 +1,72 @@
+package com.example.taobaoalliance.ui.custom;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+
+import com.example.taobaoalliance.R;
+
+public class LoadingView extends AppCompatImageView {
+
+    private float mDegrees = 0;
+    private boolean mNeedRotate = true;
+
+    public LoadingView(Context context) {
+        this(context, null);
+    }
+
+    public LoadingView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LoadingView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setImageResource(R.mipmap.loading);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mNeedRotate = true;
+        startRotate();
+    }
+
+    private void startRotate() {
+        post(new Runnable() {
+            @Override
+            public void run() {
+                mDegrees += 10;
+                if (mDegrees >= 360) {
+                    mDegrees = 0;
+                }
+                invalidate();
+                //判断是否要继续旋转
+                //如果不可见，或者已经DetachedFromWindow就不再转动了
+                if (getVisibility() != VISIBLE && !mNeedRotate) {
+                    postDelayed(this, 10);
+                } else {
+                    removeCallbacks(this);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        stopRotate();
+    }
+
+    private void stopRotate() {
+        mNeedRotate = false;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.rotate(10, getWidth() >> 2, getHeight() >> 2);
+        super.onDraw(canvas);
+    }
+}

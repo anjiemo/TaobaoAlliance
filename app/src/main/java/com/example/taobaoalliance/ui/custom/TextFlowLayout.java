aceb240a2e67397ae961ce97645ea9c50b346c78
anjiemo <26957348416@qq.com>
2020-06-29 07:31:52 +0800

    了解自定义组合控件类型（第89集）。

---
+++ app/src/main/java/com/example/taobaoalliance/ui/custom/TextFlowLayout.java
@@ -0,0 +1,25 @@
+package com.example.taobaoalliance.ui.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class TextFlowLayout extends ViewGroup {
+
+    public TextFlowLayout(Context context) {
+        super(context);
+    }
+
+    public TextFlowLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TextFlowLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}

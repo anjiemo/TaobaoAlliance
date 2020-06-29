694a7d29220c211ac5810118868f733e0d5dc190
anjiemo <26957348416@qq.com>
2020-06-29 16:09:29 +0800

    实现清明节的灰色UI（第109集）。

--- app/src/main/java/com/example/taobaoalliance/base/BaseActivity.java
+++ app/src/main/java/com/example/taobaoalliance/base/BaseActivity.java
@@ -1,6 +1,10 @@
 package com.example.taobaoalliance.base;
 
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,25 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResId());
+        //setQingMingFestivalAsh();
         mBind = ButterKnife.bind(this);
         initView();
         initEvent();
         initPresenter();
+    }
+
+    /**
+     * 设置主题为清明节灰色
+     */
+    private void setQingMingFestivalAsh() {
+        //==================清明节灰 start========================
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        Paint paint = new Paint();
+        paint.setColorFilter(new ColorMatrixColorFilter(cm));
+        View contentContainer = getWindow().getDecorView();
+        contentContainer.setLayerType(View.LAYER_TYPE_SOFTWARE, paint);
+        //==================清明节灰 end========================
     }
 
     protected abstract void initPresenter();
